dd1def41f237d4421733d33c9002ab94ab4c4e49
Daniel Naegele <danieln@selfnet.de>
2013-06-23 23:28:42 +0200

    added screenrc

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ ln -s vimrc ~/.vimrc
 ln -s tmux.conf ~/.tmux.conf
 ln -s zshrc ~/.zshrc.local
 ln -s gitconfig ~/.gitconfig
+ln -s screenrc ~/.screenrc
 wget -O ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
