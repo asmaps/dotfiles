6862a062ecc7e41b57511be30b91a79a62c55869
Daniel Naegele <danieln@selfnet.de>
2013-06-23 23:23:59 +0200

    added init script

---
+++ init.sh
@@ -0,0 +1,6 @@
+mkdir .backup
+ln -s vimrc ~/.vimrc
+ln -s tmux.conf ~/.tmux.conf
+ln -s zshrc ~/.zshrc.local
+ln -s gitconfig ~/.gitconfig
+wget -O ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
