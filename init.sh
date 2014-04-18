c23a80df61b4b35fd80d961d6f056ff2af49a935
Arne Schauf <git@as-webservices.de>
2014-04-18 12:50:35 +0200

    use my own zshrc

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ mkdir -p ~/.backup
 ln -snf `pwd`/vimrc ~/.vimrc
 ln -snf `pwd`/tmux.conf ~/.tmux.conf
 ln -snf `pwd`/zshrc.local ~/.zshrc.local
+ln -snf `pwd`/zshrc ~/.zshrc
 ln -snf `pwd`/gitconfig ~/.gitconfig
-wget -O ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
