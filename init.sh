7a04d169bc0db4237e3a288396a5241d94fbe2af
Arne Schauf <git@as-webservices.de>
2016-01-29 14:23:55 +0100

    add qtile_config.py

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.backup
+mkdir -p ~/.config/qtile
 ln -snf `pwd`/vimrc ~/.vimrc
 ln -snf `pwd`/tmux.conf ~/.tmux.conf
 ln -snf `pwd`/zshrc.local ~/.zshrc.local
 ln -snf `pwd`/zshrc ~/.zshrc
+ln -snf `pwd`/qtile_config.py ~/.config/qtile/config.py
 ln -snf `pwd`/gitconfig ~/.gitconfig
 ln -snf `pwd`/gitignore ~/.gitignore
 
