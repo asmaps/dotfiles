5e411d7a014553ea4836278e3217b575d390b90f
Arne Schauf <git@as-webservices.de>
2016-03-22 10:54:42 +0100

    init zsh on server

--- init_server.sh
+++ init_server.sh
@@ -3,3 +3,8 @@ ln -snf `pwd`/vimrc ~/.vimrc
 ln -snf `pwd`/tmux.server.conf ~/.tmux.conf
 ln -snf `pwd`/gitconfig ~/.gitconfig
 ln -snf `pwd`/gitignore ~/.gitignore
+
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+ln -snf `pwd`/ohmyzshrc ~/.zshrc
